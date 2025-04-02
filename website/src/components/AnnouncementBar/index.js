adc1048632b3255e9cabe09bc14acd5c1133ea29
Josh Struve <142850611+jstruve-catalysis@users.noreply.github.com>
2025-04-02 11:44:44 -0700

    update announcement bar

--- website/src/components/AnnouncementBar/index.js
+++ website/src/components/AnnouncementBar/index.js
@@ -10,7 +10,7 @@ const AnnouncementBar = () => {
           <div className={`col col--8 col--offset-2 ${styles.announcementBarContainer}`}>
             <div className="col-demo">
               <p className={styles.announcementBarContent}>
-              Join us Apr 1-4 at <a className={styles.announcementLink} target="_blank" href="https://azure.github.io/Cloud-Native/events/kubecon-europe-25/" data-bi-area="AnnouncementBar" data-bi-name="Announcement">KubeCon Europe 2025</a> | Give us a 🌟 <a className={styles.announcementLink} target="_blank" href="https://github.com/azure/cloud-native" data-bi-area="AnnouncementBar" data-bi-name="Give us a star on GitHub">on GitHub</a>
+              Join us Apr 22-23 at <a className={styles.announcementLink} target="_blank" href="https://focus.dotnetconf.net/" data-bi-area="AnnouncementBar" data-bi-name="Announcement">.NET Conf</a> | Give us a 🌟 <a className={styles.announcementLink} target="_blank" href="https://github.com/azure/cloud-native" data-bi-area="AnnouncementBar" data-bi-name="Give us a star on GitHub">on GitHub</a>
               </p>
             </div>
           </div>
